6e844cb850e4bd186a261d51a2618d8ba1b3878b
tradesvensk <ffkornelind@gmail.com>
2026-05-16 11:51:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -72,5 +72,5 @@ app.post('/api/order', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
